0ae0eaece7f2e71d4700cb3c0d144b0b3946a822
Hubertius <hubert.kopec@interia.pl>
2022-11-20 10:55:10 +0100

    Implementation of confitional for lab5

---
+++ lab5/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+
+void sortNumbersAscending(int number[], int count);
+void sortNumbersDescending(int number[], int count);
+
+int my_printf(char *format_string, char *param){
+	for(int i=0; i<strlen(format_string); ++i)
+	{
+		if((format_string[i] == '#') &&  isdigit((format_string[i+1])) && (format_string[i+2] == 'g'))
+		{
+
+		}
+		else
+			putchar(format_string[i]);
+	}
+	puts("");
+}
+
+int main(int argc, char *argv[]){
+	char buf[1024],buf2[1024];
+	while(gets(buf)){
+		gets(buf2);
+		my_printf(buf,buf2);
+	}
+	return 0;
+}
+
