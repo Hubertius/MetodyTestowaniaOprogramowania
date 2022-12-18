47a0dc6a8a46651c54b1592d7bdfcf9fe5edfcd3
Hubertius <hubert.kopec@interia.pl>
2022-12-18 14:12:54 +0100

    Implementation of #j and to hex conversion

--- lab7/main.c
+++ lab7/main.c
@@ -10,149 +10,44 @@ void sortNumbersDescending(int number[], int count);
 int my_printf(char *format_string, char *param){
 	for(int i=0; i<strlen(format_string); ++i)
 	{
-		if( (format_string[i] == '#') && (format_string[i+1] == '.') && isdigit((format_string[i+2])) && (format_string[i+3] == 'g'))
+		if((format_string[i] == '#') &&  (format_string[i+1] == 'j'))
 		{
 			char * end;
 			int convertedNumber = (int) (strtol(param, &end, 10));
-			bool wasNegative = false;
-			if(convertedNumber < 0)
+			if(errno == 0 && *end == '\0') // checking if conversion to number was successful)
 			{
-				convertedNumber *= -1;
-				wasNegative = true;
-			}
-			if(errno == 0 && *end == '\0') // checking if conversion to number was successful
-			{
-				int sizeOfNumber = 0;
-				if(wasNegative)
-					sizeOfNumber = strlen(param)-1;
-				else
-					sizeOfNumber = strlen(param);
-				int arr[sizeOfNumber];
+				int tmpNumber = convertedNumber;
+				int countDigits = 0;
+				do
+				{
+					tmpNumber /= 10;
+					++countDigits;
+				} while (tmpNumber != 0);
+				
+				char arr[countDigits];
+				sprintf(arr, "%x", convertedNumber);
 				int j = 0;
-				do { // filling int arr[] with digits of a converted number
-					int catchedDigit = (convertedNumber % 10);
-					catchedDigit = (catchedDigit * 9 + 1) % 10;
-					if(catchedDigit == 0)
-						arr[j] = 9;
+				while(j < strlen(arr))
+				{
+					char ch = arr[j];
+					if(ch == 'a')
+						printf("g");
+					else if(ch == 'b')
+						printf("h");
+					else if(ch == 'c')
+						printf("i");
+					else if(ch == 'd')
+						printf("j");
+					else if(ch == 'e')
+						printf("k");
+					else if(ch == 'f')
+						printf("l");
 					else
-						arr[j] = catchedDigit;
-					convertedNumber /= 10;
-					j++;
-				} while (convertedNumber != 0);
-				if(wasNegative)
-				{
-					printf("-");
-				}
-				int howManyDigitsToPrint = format_string[i+2] - '0';
-				int countOfDigits = 0;
-				if(howManyDigitsToPrint > sizeOfNumber)
-				{
-					for(int k = 0; k < howManyDigitsToPrint - sizeOfNumber; ++k)
-						printf(".");
-					howManyDigitsToPrint = sizeOfNumber;
-				}
-				for(j = sizeOfNumber-1; j >= 0 && countOfDigits < howManyDigitsToPrint; --j, ++countOfDigits)
-				{
-					printf("%d", arr[j]);
+						printf("%c", ch);
+					++j;
 				}
 			}
-			else 
-			{
-				printf("ERROR, not a number to convert");
-			}
-			i += 3;
-		}
-		else if((format_string[i] == '#') &&  isdigit((format_string[i+1])) && (format_string[i+2] == 'g'))
-		{
-			char * end;
-			int convertedNumber = (int) (strtol(param, &end, 10));
-			bool wasNegative = false;
-			if(convertedNumber < 0)
-			{
-				convertedNumber *= -1;
-				wasNegative = true;
-			}
-			if(errno == 0 && *end == '\0') // checking if conversion to number was successful
-			{
-				int sizeOfNumber = 0;
-				if(wasNegative)
-					sizeOfNumber = strlen(param)-1;
-				else
-					sizeOfNumber = strlen(param);
-				int arr[sizeOfNumber];
-				int j = 0;
-				do { // filling int arr[] with digits of a converted number
-					int catchedDigit = (convertedNumber % 10);
-					catchedDigit = (catchedDigit * 9 + 1) % 10;
-					if(catchedDigit == 0)
-						arr[j] = 9;
-					else
-						arr[j] = catchedDigit;
-					convertedNumber /= 10;
-					j++;
-				} while (convertedNumber != 0);
-				if(wasNegative)
-				{
-					printf("-");
-				}
-				int howManyDigitsToPrint = format_string[i+1] - '0';
-				int countOfDigits = 0;
-				if(howManyDigitsToPrint > sizeOfNumber)
-				{
-					for(int k = 0; k < howManyDigitsToPrint - sizeOfNumber; ++k)
-						printf("_");
-					howManyDigitsToPrint = sizeOfNumber;
-				}
-				for(j = sizeOfNumber-1; j >= 0 && countOfDigits < howManyDigitsToPrint; --j, ++countOfDigits)
-				{
-					printf("%d", arr[j]);
-				}
-			}
-			else 
-			{
-				printf("ERROR, not a number to convert");
-			}
-			i += 2;
-		}
-		else if((format_string[i] == '#') &&  (format_string[i+1] == 'g'))
-		{
-			char * end;
-			int convertedNumber = (int) (strtol(param, &end, 10));
-			bool wasNegative = false;
-			if(convertedNumber < 0)
-			{
-				convertedNumber *= -1;
-				wasNegative = true;
-			}
-			if(errno == 0 && *end == '\0') // checking if conversion to number was successful
-			{
-				int sizeOfNumber = 0;
-				if(wasNegative)
-					sizeOfNumber = strlen(param)-1;
-				else
-					sizeOfNumber = strlen(param);
-				int arr[sizeOfNumber];
-				int j = 0;
-				do { // filling int arr[] with digits of a converted number
-					int catchedDigit = (convertedNumber % 10);
-					catchedDigit = (catchedDigit * 9 + 1) % 10;
-					if(catchedDigit == 0)
-						arr[j] = 9;
-					else
-						arr[j] = catchedDigit;
-					convertedNumber /= 10;
-					j++;
-				} while (convertedNumber != 0);
-				if(wasNegative)
-				{
-					printf("-");
-				}
-				for(j = sizeOfNumber-1; j >= 0; --j)
-				{
-					printf("%d", arr[j]);
-				}
-			}
-			else 
+			else
 			{
 				printf("ERROR, not a number to convert");
 			}
